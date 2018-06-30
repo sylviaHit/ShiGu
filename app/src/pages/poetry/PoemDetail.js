e0528a38ba948f2de5d5db06e4e63482f411fbb4
duwendi <987638631@qq.dom>
2018-06-30 18:26:57 +0800

    诗词查询功能添加

--- app/src/pages/poetry/PoemDetail.js
+++ app/src/pages/poetry/PoemDetail.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 import {
     StyleSheet,
     View,
-    Text
+    Text,
+    TextInput,
+    TouchableOpacity
 } from 'react-native';
 import {service} from "../../utils/service";
 
@@ -15,8 +17,11 @@ export default class PoemDetail extends Component {
             title: '',
             subTitle: '',
             preface: '',
-            content: ''
+            content: '',
+            showValue: ''
         }
+        // 诗词查询输入框
+        this.onChangeText = this.onChangeText.bind(this);
     }
 
     componentWillMount() {
@@ -53,6 +58,35 @@ export default class PoemDetail extends Component {
         });
     };
 
+    onChangeText=(inputData)=>{
+        this.setState({showValue: inputData});
+    }
+
+    showData=()=>{
+        alert(this.state.showValue);
+        service.get('https://api.sou-yun.com/api/poem', {key: this.state.showValue, jsonType: true}).then((response) => {
+            if (response.ShiData && response.ShiData.length > 0) {
+
+                let data = response.ShiData[0];
+                let title = data.Title.Content;
+                let subTitle = '（' + data.Dynasty + '.' + data.Author + '）';
+                let preface = data.Preface;
+                let content = '';
+                if(data.Clauses && data.Clauses.length>0){
+                    data.Clauses.forEach((item,index)=>{
+                        if(index%2 == 1){
+                            content += item.Content + '\n';
+                        }else{
+                            content += item.Content;
+                        }
+
+                    });
+                }
+                me.setState({title: title, preface: preface,content: content,subTitle: subTitle});
+            }
+        });
+    }
+
     componentWillReceiveProps(nextProps) {
         this.setState({
             id: nextProps.navigation && nextProps.navigation.state && nextProps.navigation.state.params && nextProps.navigation.state.params.id || ''
@@ -62,8 +96,10 @@ export default class PoemDetail extends Component {
     render() {
         let me = this;
         const {id, title, subTitle, preface, content,} = me.state;
+        let inputTag = <TextInput placeholder='请输入想查询的关键字' editable={true} style={styles.inputstyle} onChangeText={this.onChangeText}></TextInput>;
+        let touchableTag = <TouchableOpacity onPress={this.showData.bind(this)}><View style={styles.btn}><Text style={styles.wordc}>搜索</Text></View></TouchableOpacity>
         return (
-            id ?<View style={styles.container}><Text Style={styles.allTitle}><Text style={styles.title}>{title}</Text><Text>{subTitle}</Text></Text><Text style={styles.preface}>{preface}</Text><Text style={styles.content}>{content}</Text>
+            id ?<View style={styles.container}>{inputTag}{touchableTag}<Text Style={styles.allTitle}><Text style={styles.title}>{title}</Text><Text>{subTitle}</Text></Text><Text style={styles.preface}>{preface}</Text><Text style={styles.content}>{content}</Text>
             </View>:<View style={styles.container}><Text style={{marginTop: 10}}>暂无数据</Text></View>)
     }
 }
@@ -91,5 +127,28 @@ const styles = StyleSheet.create({
     content: {
         marginTop: 10,
         lineHeight:28
+    },
+    mycontainer:{
+        marginTop:30,
+        flexDirection:"row",
+    },
+    inputStyle:{
+        width:280,
+        height:30,
+        borderColor:"black",
+        borderWidth:1,
+        marginLeft:5,
+    },
+    btn:{
+        width:85,
+        height:30,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"green",
+        // borderRadius:5
+    },
+    wordC:{
+        color:"white",
+        fontSize:18,
     }
 });
